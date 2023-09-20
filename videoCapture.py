8a0f1a3e7944e892dcded2de4529c9aaca00f550
BMugo84 <mugo.benson.kbm@gmail.com>
2023-09-20 18:47:48 +0300

    sample recording of video

--- videoCapture.py
+++ videoCapture.py
@@ -5,6 +5,15 @@ cam = cv2.VideoCapture(1)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade =  cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
+recording = False
+
+# set video height and width 
+frame_size = (int(cam.get(3)), int(cam.get(4)))
+# init an mp4 codec compressor
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+# output the file / write
+out = cv2.VideoWriter(filename="video.mp4", fourcc=fourcc, fps=20, frameSize=frame_size)
+
 while cam.isOpened():
 
     ret, frame = cam.read()
@@ -13,23 +22,19 @@ while cam.isOpened():
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(image=gray, scaleFactor=1.3, minNeighbors=5)
     eyes = eye_cascade.detectMultiScale(image=gray, scaleFactor=1.3, minNeighbors=5)
+
+    if len(faces) + len(eyes) > 0:
+        recording = True
     
-
-    # draw rectangle 
-    # over face
-    for fx, fy, fwidth, fheight in faces:
-        cv2.rectangle(img=frame, pt1=(fx, fy), pt2=(fx+fwidth, fy+fheight), color=(0,255,0), thickness=2)
-
-    #over eyes
-    for ex, ey, ewidth, eheight in eyes:
-        cv2.rectangle(img=frame, pt1=(ex, ey), pt2=(ex+ewidth, ey+eheight), color=(255,0,0), thickness=2)
-
-
+    # write the frame 
+    out.write(frame)
         
     cv2.imshow('Sec Cam', frame)
 
     if cv2.waitKey(10) == ord('q'):
         break
 
+#  quit recording
+out.release()
 cam.release()
 cv2.destroyAllWindows()
